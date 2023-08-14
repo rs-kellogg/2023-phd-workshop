18ba6f932761e07d1d709e94ecda175a9a3798b3
William Thompson <will.k.t@gmail.com>
2023-08-14 08:13:14 -0500

    updates

--- session-2/python-example/openaihelper/cli.py
+++ session-2/python-example/openaihelper/cli.py
@@ -22,10 +22,16 @@ app = typer.Typer()
 
 logging.basicConfig(filename="openai-helper.log", encoding="utf-8", level=logging.INFO)
 
-# model_name = "gpt-3.5-turbo"
-# encoding_name = "cl100k_base"
-# max_token_len = 4097
-# prompt = "Read through the newspaper text and identify the following: environmental movement organization (EMO) involved, action taken by EMO, target of the action, which of the following strategies is being used- verbal statements, political tactic, education/raising awareness, juridicial tactics, disruptive protests, lifestyle/culture tactics, direct environmental protection, nondisruptive protest, and affecting business-, whether it is collaborative or contentious action, and where the event/interaction is happening. Answer in the following format. EMO: ; action: ; target: ; strategy: ; collaborative/contentious: ; city: ; county: ; state: . If the article is an advertisement, editorial, or opinions, or if it reports multiple unrelated events or non-local, international ones, return NA and specify why. Newspaper text: "
+
+def check_args(
+    data_file_path: Path,
+    config_file_path: Path,
+    outdir: Path,
+):
+    assert data_file_path.exists()
+    assert config_file_path.exists()
+    if not outdir.exists():
+        outdir.mkdir(parents=True)
 
 
 @app.command()
@@ -34,25 +40,30 @@ def count_tokens(
     config_file_path: Path = typer.Argument(..., help="Config file path name"),
     outdir: Path = typer.Option(Path("."), help="Output directory"),
 ):
-    assert data_file_path.exists()
-    assert config_file_path.exists()
-    if not outdir.exists():
-        outdir.mkdir(parents=True)
+    """
+    Count tokens in a data file and write output to a csv file.
+    """
+    check_args(data_file_path, config_file_path, outdir)
 
+    # Read config file
     config = F.config(config_file_path)
     encoding_name = config["encoding_name"]
     max_token_len = config["max_token_len"]
 
+    # Read data file
     df = pd.read_csv(data_file_path)
     assert "id" in df.columns
     assert "text" in df.columns
     texts = list(df["text"])
 
+    # Count tokens
     out_df = df[["id"]].copy()
     counts = []
     for i in tqdm(range(len(texts))):
         counts.append(F.count_tokens(texts[i], encoding_name))
     out_df["count"] = counts
+
+    # Write output
     out_df.to_csv(
         f"{outdir}/{data_file_path.stem}_counts.csv", 
         quoting=csv.QUOTE_ALL,
@@ -61,27 +72,53 @@ def count_tokens(
 
 
 @app.command()
-def process(
-    data_file: str,
-    openai_api_key_file: str,
-    outdir: str = ".",
+def complete_prompt(
+    data_file_path: Path = typer.Argument(..., help="Data file path name"),
+    config_file_path: Path = typer.Argument(..., help="Config file path name"),
+    outdir: Path = typer.Option(Path("."), help="Output directory"),
 ):
-    openai.api_key = Path(openai_api_key_file).read_text()
-    df = pd.read_csv(data_file)
-    indices = list(df.index)
+    """
+    Complete a prompt in a data file and write output to a csv file.
+    """
+    check_args(data_file_path, config_file_path, outdir)
+
+    # Read config file and setup OpenAI
+    config = F.config(config_file_path)
+    api_key = Path(config["openai_api_key_file"]).read_text()
+    encoding_name = config["encoding_name"]
+    max_token_len = config["max_token_len"]
+    model_name = config["model_name"]
+    prompt = config["prompt"]
+    n_prompt_tokens = F.count_tokens(prompt, encoding_name)
+    openai.api_key = api_key
+
+    # Read data file
+    df = pd.read_csv(data_file_path)
+    assert "id" in df.columns
+    assert "text" in df.columns
     texts = list(df["text"])
-    counts = list(df["count"])
+    ids = list(df["id"])
+
+    # Process texts
+    out_df = df[["id"]].copy()
     responses = []
-    for i in tqdm(range(len(texts))):
-        if counts[i] > max_token_len:
+    for i in tqdm(range(len(ids))):
+        n_tokens = F.count_tokens(texts[i], encoding_name)
+        if (n_tokens + n_prompt_tokens) > max_token_len:
             responses.append("TOO_LONG")
         else:
             responses.append(
                 str(F.chat_complete(texts[i], model_name=model_name, prompt=prompt))
             )
-        logging.info(f"{indices[i]}: {responses[i]}")
-    df["responses"] = responses
-    df.to_csv(f"{outdir}/{data_file}_responses.csv", quoting=csv.QUOTE_ALL)
+        logging.info(f"Data point {ids[i]} completed")
+    out_df["response"] = responses
+
+    # Write output
+    out_df.to_csv(
+        f"{outdir}/{data_file_path.stem}_response.csv", 
+        quoting=csv.QUOTE_ALL,
+        index=False,
+    )
 
 
 if __name__ == "__main__":
