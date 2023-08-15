a202cb4c86ed689be889e53cd7f5c5d3fb3e0261
William Thompson <will.k.t@gmail.com>
2023-08-14 19:45:30 -0500

    udpates

--- session-2/python-example/tests/test_cli.py
+++ session-2/python-example/tests/test_cli.py
@@ -1,1 +1,20 @@
+import os
+import tempfile
+from typer.testing import CliRunner
+from openaihelper.cli import *
+import pytest
+
+runner = CliRunner()
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.mark.skip(reason="not working")
+def test_cli(tmpdir):
+    result = runner.invoke(
+        app,
+        ["count-tokens", "sample-100.csv", "config.yml", "--outdir", str(tmpdir)],
+    )
+    assert result.exit_code == 0
+    assert result.stdout.count("processing file:") == 100
 
