98d6ff417236b60b82f18d28355ad1ee5aaebbbe
John Johnson <johnpjohnson@users.noreply.github.com>
2023-08-16 21:10:00 -0500

    Reformat activity log, remove unused packages

--- session-7-qualtrics/qualtRics_demo.R
+++ session-7-qualtrics/qualtRics_demo.R
@@ -3,31 +3,21 @@
 ############################
 
 # Set Global Parameters
-outdir = "downloads"  # specified as a relative path in working dir
-
-# Clear Workspace
-rm(list=ls()) 
+outdir <- "/kellogg/proj/jpj8711/qualtrics/"
+sname  <- "Response Pilot 2023 (Prolific)"
 
 # Install and load necessary libraries
 if(!require("qualtRics")) install.packages("qualtRics")
 if(!require("dplyr")) install.packages("dplyr")
 if(!require("stringr")) install.packages("stringr")
-if(!require("rmarkdown")) install.packages("rmarkdown")
-if(!require("vtable")) install.packages("vtable")
-if(!require("stargazer")) install.packages("stargazer")
 if(!require("data.table")) install.packages("data.table")
 if(!require("formattable")) install.packages("formattable")
-if(!require("vcd")) install.packages("vcd")
 
 library(qualtRics)
 library(dplyr)
 library(stringr)
-library(rmarkdown)
-library(vtable)
-library(stargazer)
 library(data.table)
 library(formattable)
-library(vcd)
 
 ## RUN THIS STEP FIRST TIME ONLY, TO GENERATE ".Renviron" FILE
 #qualtrics_api_credentials(api_key = "INSERT YOUR API KEY HERE",
@@ -36,8 +26,17 @@ library(vcd)
 #                          overwrite = TRUE)
 readRenviron("~/.Renviron")
 
+# If necessary, create directory where output will be saved
 if (!dir.exists(outdir)) {
   dir.create(outdir)
+}
+
+# If necessary, create an activity log file
+logfile = paste(outdir,"/activity_log.txt",sep="")
+if (!file.exists(logfile)){
+  fileConn <- file(logfile)
+  writeLines("Date|SurveyID|SurveyName|nrows", fileConn)
+  close(fileConn)
 }
 
 # Show all surveys 
@@ -45,8 +44,7 @@ surveys   <- all_surveys()
 surveys
 
 # Select one project by name
-myproject <- filter(surveys, name=="Response Pilot 2023 (Prolific)")
-myproject
+myproject <- filter(surveys, name==sname)
 myproject$id[1]
 
 #Fetch the survey for that project as .RDS file
@@ -56,21 +54,19 @@ mysurvey <- fetch_survey(surveyID = myproject$id[1],
                          verbose = TRUE)
 nrow(mysurvey)
 
-# save the approximate day/time of when you downloaded the survey
-Sys.time()
-download_time <- format(Sys.time(), "%a %b %d %X %Y")
-download_message <- paste("The ", myproject$name, "survey data is called ", myproject$id[1] , ".rds and was downloaded on: ", download_time, sep="")
-download_message
-
-# save download time to log file
-logfile_path <- paste(outdir, "/logfile.txt", sep="")
-writeLines(download_message, logfile_path)
 
 # Read RDS file into dataframe "mysurvey", and save copy as CSV
 rdspath <- paste(outdir, "/", myproject$id[1], ".rds", sep="")
 mysurvey <- readRDS(file = rdspath)
-csvpath <- paste(outpath, "/", myproject$id[1], ".csv", sep="")
+csvpath <- paste(outdir, "/", myproject$id[1], ".csv", sep="")
 ret <- write.csv(x=mysurvey, file=csvpath)
+
+# Write an entry to activity log file
+
+stringout = ""
+stringout = paste(Sys.Date(),csvpath,sname,nrow(mysurvey),sep="|")
+stringout
+write(stringout,file=logfile,append=TRUE)
 
 
 # Fix problem of spaces in column names!
@@ -78,15 +74,18 @@ names(mysurvey) <- str_replace_all(names(mysurvey),c(" "=".", ","=""))
 names(mysurvey)[names(mysurvey) == "Duration.(in.seconds)"] <- "Duration"
 colnames(mysurvey)
 
+View(mysurvey)
+
 ### FILTERING
 # Eliminate test responses
 mysample <- filter(mysurvey, Q5 != "delete me")
 
+
 # Function to return total respondents and % who are female
 sample_count <- function(a) {
   females <- filter(a, Gender == "Female")
-  pct_female <- (nrow(females) / nrow(a))
-  return = paste("The sample now contains ", nrow(a), " reponses, ", percent(pct_female,1), " of which are from subjects who identify as female.", sep="", collapse=NULL) 
+  pct_female <- percent((nrow(females) / nrow(a)),1)
+  return = paste("The sample now contains ", nrow(a), " reponses, ", pct_female, " of which are from subjects who identify as female.", sep="", collapse=NULL) 
   return
 }
 sample_count(mysample)
@@ -112,8 +111,8 @@ mysample3b <- filter(mysample3b, `Duration` < quant_duration[20])
 sample_count(mysample3b)
 
 # (c) using mean and standard deviation
-print(mean(mysample2$Duration))
-print(  sd(mysample2$Duration))
+#print(mean(mysample2$Duration))
+#print(  sd(mysample2$Duration))
 mysample3c <- filter(mysample2, `Duration` < mean(mysample2$Duration) + 1.645*sd(mysample2$Duration))
 print("We removed responses that were more than 1.645 standard deviations beyond mean response time.")
 sample_count(mysample3c)
